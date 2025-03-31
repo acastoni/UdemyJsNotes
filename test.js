feddaf9c23b42bd6621b853de403f1d523330335
acastoni <acast698@fiu.edu>
2025-03-30 23:22:30 -0400

    udemy notes

---
+++ test.js
@@ -0,0 +1,17 @@
+//Simple for loop syntax example
+for (i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+//Opens the alert dialog box with the message "Hello World!"
+// alert("Hello World!"); //doesn't work in node.
+
+let js = "amazing";
+
+if (js === "amazing") console.log("JavaScript is amazing!");
+
+console.log(40 + 8 + 23 - 10);
+
+/* JavaScript is a high-level,
+object-oriented,
+multi-paradigm, programming language */
